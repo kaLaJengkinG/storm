219f7b89e0ac4311bc293113c8277a856a07a1ab
arnold.lapian <arnold.lapian@87f45496-1afc-11df-8134-2392cdf085bb>
2011-01-04 19:21:40 +0000

    Fix bug at roster_plugin.py

--- plugins/roster_plugin.py
+++ plugins/roster_plugin.py
@@ -23,12 +23,17 @@ def roster_unsub(type,source,parameters):
 
 def roster_show(type,source,parameters):
         ROSTER = JCON.getRoster()
-        s=''
+        list, col = '', 0
         rep = ROSTER.getItems()
-        for x in rep:
-                s+=x+' : '+unicode(ROSTER.getShow(x))+'\n'
-        reply(type,source, s)
-
+        for jid in rep:
+                if not jid.count('@conf'):
+                    col = col + 1
+                    list += '\n'+str(col)+'. '+jid
+        if col != 0:
+                reply(type, source, (u'\nTotal: %s contacts in my roster:' % str(col))+list)
+        else:
+                reply(type, source, u'My roster is empty...')
+		
 register_command_handler(roster_show, 'roster_show', ['superadmin','all'], 100, 'Show contacts on bot roster.', 'roster_all', ['roster_show'])		
 register_command_handler(roster_sub, 'roster_add', ['superadmin','all'], 100, 'Add a contact on bot roster.', 'roster_add <jid>', ['roster_add guy@server.tld'])
 register_command_handler(roster_unsub, 'roster_del', ['superadmin','all'], 100, 'Delete a contact on bot roster.', 'roster_del <jid>', ['roster_del guy@server.tld'])
